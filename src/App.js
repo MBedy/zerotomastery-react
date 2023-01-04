38b219e926af365163a97f4e739190e519b2056a
M_Bedy <marcelin.bedy@gmail.com>
2023-01-04 12:30:06 +0100

    Section 8 : lesson 11

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-import { useContext } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-
-import { UserContext } from "./contexts/user.context";
+import { Routes, Route } from "react-router-dom";
 
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
@@ -12,18 +9,12 @@ const Shop = () => {
 };
 
 const App = () => {
-  const { currentUser } = useContext(UserContext);
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
-        <Route
-          path="auth"
-          element={
-            currentUser ? <Navigate to="/" replace /> : <Authentication />
-          }
-        />
+        <Route path="auth" element={<Authentication />} />
       </Route>
     </Routes>
   );
